e3a9bde0ded3accd6380f62cae503cfd7e5ec560
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-07 11:33:54 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,36 @@
+## Variables : Variable is a container for a value (String, integer, float, boolean)
+
+#Strings
+first_name = "Veeresh"
+print(first_name")
+print(f"Your name is: {first_name}")
+food = "pizza"
+print(f"Your fav food is: {food}")
+
+
+#Integers
+age = 25
+print(f"You're {age} years old)
+students = 60
+print(f"In a class, there are {students} students")
+
+
+#Float
+price = 2.99
+print(f"The price is: {price}")
+balance = 3999.30
+print(f"Your acc balance is: {balance}")
+
+
+# Boolean
+isAlive = True
+if isAlive:
+  print("You're a alive")
+else:
+  print("You're DEAD")
+  
+sale = True
+if sale:
+  print("Yes for SALE")
+else:
+  print("NOT for SALE")
