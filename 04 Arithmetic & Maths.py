9a5b50cd9aedb779446e36a32ece8f8bf6996cd5
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-07 12:46:55 +0530

    Create 04 Arithmetic & Maths.py

---
+++ 04 Arithmetic & Maths.py
@@ -0,0 +1,56 @@
+friends = 0
+friends += 3
+print(friends)
+
+num1 = 10
+num1 -= 12
+print(num1)
+
+num2 = 2
+num2 *= 3
+print(num2)
+
+num3 = 10
+num3 /= 2
+print(num3)
+
+num4 = 2
+num4 **= 5
+print(num4)
+
+num5 = 10
+num5 %= 3
+print(num5)
+
+
+
+w = -2
+x = 3.14
+y = 4
+z = 5
+
+result1 = round(x)
+print(result1)
+
+result2 = abs(w)
+print(result2)
+
+result3 = pow(y,3)
+print(result3)
+
+result4 = max(w,x,y,z)
+print(result4)
+
+result5 = min(w,x,y,z)
+print(result5)
+
+import math
+
+x = 16
+y = 2.3
+
+print(math.pi)
+print(math.e)
+print(math.sqrt(x))
+print(math.ceil(y))
+print(math.floor(y))
