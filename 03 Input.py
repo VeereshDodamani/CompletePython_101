66aeed495532baeb4336cd452d5257b83a7069f9
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-07 11:50:02 +0530

    Create 03 Input.py

---
+++ 03 Input.py
@@ -0,0 +1,7 @@
+## input() : A function that prompts the user to enter the data and return the data as a string 
+
+name = input("Enter your name here: ")
+age = input("What is your age? ")
+
+print(f"Hello, {name}")
+print(f"Wowww, you're {age} years old")
