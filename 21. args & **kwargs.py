50ecc582557856f65ad51b4ceed0a0557d957c97
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-14 23:08:16 +0530

    Create 21. args & **kwargs.py

---
+++ 21. args & **kwargs.py
@@ -0,0 +1,4 @@
+# *args = allows to pass multiple non-key arguments
+# **kwargs = allows to pass multiple keywords arguments
+#            * unpacking arguments
+#            1. positional, 2.default, 3.keyowrds, 4. Arbitrary
