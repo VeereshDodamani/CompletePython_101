e6598e099208c0e159cc1b104fda0090a9bddb29
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-15 00:22:28 +0530

    Update 21. args & **kwargs.py

--- 21. args & **kwargs.py
+++ 21. args & **kwargs.py
@@ -27,5 +27,31 @@ print(add(1,2,3,5,6))
 def display_name(*name):
     for nam in name:
         print(nam, end=" ")
-
 display_name("Dr.","Hari","Prasad","III")
+
+
+# **keargs conver the arguments into dictionaries
+def print_adderess(**kwargs):
+    print(type(kwargs))
+    for key, value in kwargs.items():
+        print(f"{key} : {value}")
+
+
+print_adderess(street="123 Fake Str",
+      city="Banglore",
+      state="Washing_ton",
+      zip="354798")
+
+
+def shipping_label(*args, **kwargs):
+    for name in args:
+        print(name, end=" ")
+    print("\n")
+    
+    for key, value in kwargs.items():
+        print(f"{key} : {value}")
+
+shipping_label("Dr.","Hari","Prasad",
+               street = "123 Fake road",
+               city = "Anom City",
+               pin = "431209")
