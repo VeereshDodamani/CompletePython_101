188fbfbf41115e1ed50e73af7b0f85f245087697
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-15 22:06:27 +0530

    Update 22. Iterables.py

--- 22. Iterables.py
+++ 22. Iterables.py
@@ -1,1 +1,21 @@
+## Iterables : An objest/collection that can return it's element one at a time,
+#              allowing it to be iterable over ina loop
 
+# List are iterables
+print("LISTS")
+nums = [1,2,3,4,5]
+for num in nums:
+    print(num, end=" ")
+print("\n")
+
+for num in reversed(nums):
+    print(num, end=" ")
+print("\n")
+
+
+# Tuples are iterables
+print("TUPLES")
+items = (1,2,3,4,5)
+for item in items:
+    print(item, end=" ")
+print("\n")
