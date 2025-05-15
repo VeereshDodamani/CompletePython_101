26546366951a3f93c110ae9e48677267c115bc77
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-16 00:34:57 +0530

    Create 24. List comprehension.py

---
+++ 24. List comprehension.py
@@ -0,0 +1,32 @@
+## List comprehension : A concise way to create lists in python
+#                       compact and easy to read than traditional loop
+#                       [expression for value in iterable if condition]
+
+# normal way to create a list
+print("By using normal way")
+doubles = []
+for x in range(1,11):
+    doubles.append(x*2)
+print(doubles)
+
+# using the comperhension
+print("By using comperhension")
+doubles = [x*2 for x in range(1,11)]
+print(doubles)
+
+fruits = ["apple","banana","watermelon","pineapple"]
+fruits = [fruit.upper() for fruit in fruits]
+print(fruits)
+
+fruits = [fruit.upper() for fruit in ["apple","banana","watermelon","pineapple"]]
+print(fruits)
+
+fruit_char = [fruit[0] for fruit in fruits]
+print(fruit_char)
+
+nums = [-2,-5,8,9,-4,2,7,0,-6,1]
+pos_num = [number for number in nums if number>=0]
+print(pos_num)
+
+neg_num = [number for number in nums if number<0]
+print(neg_num)
