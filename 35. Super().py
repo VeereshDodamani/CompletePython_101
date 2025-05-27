6bb9514a83fbf68a36a89730f68b854357f90128
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-27 17:09:15 +0530

    Update 35. Super().py

--- 35. Super().py
+++ 35. Super().py
@@ -22,3 +22,38 @@ class Triangle:
 
 # Now suppose if you change the variable name of filled with is_filled then you need to change it everywhere manually.
 # That's where super() or inheritance can come in function
+
+class Shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+circle = Circle(color="Red",is_filled=True, radius=5)
+print("Circle info: ")
+print("Circle color: ",circle.color)
+print("IS filled: ",circle.is_filled)
+print("Radius of circle: ",circle.radius)
+print("\n")
+
+triangle = Triangle(color="Blue",is_filled=False, width=10, height=12)
+print("Triangle info: ")
+print("Triangle color: ",triangle.color)
+print("IS filled: ",triangle.is_filled)
+print("Width of Triangle: ", triangle.width)
+print("Height of Triangle: ",triangle.height)
