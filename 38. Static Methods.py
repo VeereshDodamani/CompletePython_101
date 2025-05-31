234d3fa6371664e607de1bc664a0ef0e0bbb175d
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-31 12:37:37 +0530

    Create 38. Static Methods.py

---
+++ 38. Static Methods.py
@@ -0,0 +1,5 @@
+# Static Methods : A emthod that belong to a class rather than a object from the class (instance)
+#                  Used uaually for general utility function
+
+# Instance method : Best for operations on instances of the class
+# Static method : Best for utility functions that do not need access to class data
