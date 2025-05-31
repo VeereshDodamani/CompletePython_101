3ffaecbdab7a294b3fcc4dbd24b4f69a0e492505
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-05-31 07:33:30 +0530

    Update 36. Polymorphism.py

--- 36. Polymorphism.py
+++ 36. Polymorphism.py
@@ -6,19 +6,40 @@
 #           1. Inheritance = An object could be treated of same type as a parent class
 #           2. "Duck Typing" = Object must have necessary attributes/methods
 
+from abc import ABC, abstractclassmethod
 class Shape:
-    pass
+    @abstractclassmethod
+    def area(self):
+        pass
 
 class Circle(Shape):
-    pass
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14*self.radius**2
 
 class Square(Shape):
-    pass
+    def __init__(self,width):
+        self.width = width
+    
+    def area(self):
+        return self.width**2
 
 class Triangle(Shape):
-    pass
+    def __init__(self,height,base):
+        self.height = height
+        self.base = base
+    
+    def area(self):
+        return 0.5*self.base*self.height
 
 
 # square is Square, square is Shape, but not circle, triangle
 # square have two forms : Square and Shape
-square = Square()
+# square = Square()
+
+shapes = [Circle(4), Square(5), Triangle(4,5)]
+
+for shape in shapes:
+    print(f"The area of {shape.area()} cm²")
