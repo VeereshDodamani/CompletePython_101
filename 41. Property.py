439c21ca4527976efbb3d88e85b1b9bb72871788
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-06-02 16:16:14 +0530

    Update 41. Property.py

--- 41. Property.py
+++ 41. Property.py
@@ -8,6 +8,7 @@ class Rectangle:
     def __init__(self, width, height):
         self._width = width # Private width
         self._height = height # Private height
+
 # getter method
     @property
     def width(self):
@@ -17,9 +18,45 @@ class Rectangle:
     def height(self):
         return f"{self._height:.1f}cm"
 
+# setter method
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
+        else:
+            print("Width must be > 0")
+            print("\n")
+
+    @height.setter
+    def height(self, new_height):
+        if new_height > 0:
+            self._height = new_height
+        else:
+            print("Height must be > 0")
+            print("\n")
+
+# deleter method
+    @width.deleter
+    def width(self):
+        del self._width
+        print("Width have been deleted")
+    
+    @height.deleter
+    def height(self):
+        del self._height
+        print("Height have been deleted")
+
 
 rectange = Rectangle(3,4)
+
+rectange.width = 0
+rectange.height = 5
+
 print("Getting the direct private variable")
 print(f"Width : {rectange._width}, Height : {rectange._height}")
 print("Using the getter method")
 print(f"Width : {rectange.width}, Height : {rectange.height}")
+print("\n")
+
+del rectange.width
+del rectange.height
