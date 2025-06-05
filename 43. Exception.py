ccfe60d6003547d7cc670c4815b4dd4d3adfcba3
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-06-05 07:46:37 +0530

    Create 43. Exception.py

---
+++ 43. Exception.py
@@ -0,0 +1,18 @@
+# Exception : An event that interupt the flow of event
+#             (ZeroDivision, TypeError, ValueError)
+#             1. try, 2. except, 3. finally
+
+# if the input is not number then this will crash
+# num = int(input("Enter a number: "))
+# print(1/num)
+
+# to overcome that
+try:
+    num = int(input("Enter a number: "))
+    print(1/num)
+# if it is any string
+except ValueError:
+    print("Enter a valid input.")
+# if it is 0
+except ZeroDivisionError:
+    print("You can't divide by 0.")
