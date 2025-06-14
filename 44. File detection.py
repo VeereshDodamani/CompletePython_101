df4f8dbe91749a55526ba63298d952d3fce5afc6
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-06-14 15:23:13 +0530

    Update 44. File detection.py

--- 44. File detection.py
+++ 44. File detection.py
@@ -2,8 +2,17 @@
 
 import os
 
-file_path = "test.txt"
+file_path = "C:/User/HP/Desktop/test.txt"
+#file_path = "test.txt"
 if os.path.exists(file_path):
     print(f"The location {file_path} exist.")
+
+    # To check if it is a file, not a directory
+    if os.path.isfile(file_path):
+        print("That is a file.")
+
+    # To check if it is a directory
+    elif os.path.isdir(file_path):
+        print("That is a directory.")
 else:
     print(f"The location {file_path} doesn't exist.")
