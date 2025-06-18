164ec275afd6fe0f4fbc1e3c0eaa9290ccb35fc4
Veeresh R Dodamani <135006390+VeereshDodamani@users.noreply.github.com>
2025-06-18 10:54:02 +0530

    Update 49. MultiThreading.py

--- 49. MultiThreading.py
+++ 49. MultiThreading.py
@@ -4,23 +4,24 @@
 import threading
 import time
 
-def walk_dog():
-    time.sleep(10)
-    print("You finish walking the DOG.")
+def walk_dog(first):
+    time.sleep(8)
+    print(f"You finish walking {first}")
 
 def take_out_trash():
-    time.sleep(3)
+    time.sleep(6)
     print("Take out the trash.")
 
 def get_mail():
-    time.sleep(5)
+    time.sleep(3)
     print("You will get the mail")
 
 # what it does is
 # once when the time starts, after 3 sec take_out_trash() will be execuated, after 5 sec get_mail() and then after 10 sec walk_dog
 # time won't start from beginning after every function
 
-chore1 = threading.Thread(target=walk_dog)
+# to pass a argument
+chore1 = threading.Thread(target=walk_dog, args=("Scooby",)) # pass it as tuple
 chore1.start()
 
 chore2 = threading.Thread(target=take_out_trash)
@@ -28,3 +29,9 @@ chore2.start()
 
 chore3 = threading.Thread(target=get_mail)
 chore3.start()
+
+chore1.join()
+chore2.join()
+chore3.join()
+
+print("All chore's completed")
